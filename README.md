693d79c2e9028f10ceff794bbceb7e32f5c572d6
nadinananda <naadinananda@gmail.com>
2025-11-01 14:34:04 +0700

    Setup Flutter

---
+++ README.md
@@ -0,0 +1,5 @@
+# Street Corner
+
+## Tugas 7 
+### 1. 
+
---
+++ android/app/src/main/kotlin/com/example/street_corner/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.street_corner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
